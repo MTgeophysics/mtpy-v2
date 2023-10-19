a0eba4099f28c9dfa4377bd86338a07d1196a33e
JP <peacock.jared@gmail.com>
2023-10-19 13:13:12 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,6 +65,8 @@ author = "Jared Peacock, Alison Kirkby, Fei Zhang, Lars Krieger, Stephan Thiel"
 # for |version| and |release|, also used in various other places throughout
 # the built documents.
 #
+
+import mtpy
 # The short X.Y version.
 version = "2.0.0"
 # The full version, including alpha/beta/rc tags.
