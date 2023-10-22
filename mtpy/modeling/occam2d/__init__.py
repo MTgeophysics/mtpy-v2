09851fa686ec23821ae7c5904f3160c3981d833e
JP <peacock.jared@gmail.com>
2023-10-22 16:13:08 -0700

    Update __init__.py

--- mtpy/modeling/occam2d/__init__.py
+++ mtpy/modeling/occam2d/__init__.py
@@ -2,7 +2,6 @@ from .mesh import Mesh
 from .regularization import Regularization
 from .startup import Startup
 from .data import Occam2DData
-from .response import Response
 from .model import Occam2DModel
 
 __all__ = [
@@ -10,6 +9,5 @@ __all__ = [
     "Regularization",
     "Startup",
     "Occam2DData",
-    "Response",
     "Occam2DModel",
 ]
