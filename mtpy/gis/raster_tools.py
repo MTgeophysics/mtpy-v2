a3a2019624db58d180b193a7eabc4f3abe9b3060
JP <peacock.jared@gmail.com>
2023-11-03 16:42:37 -0700

    adding rasterio raster writing.

--- mtpy/gis/raster_tools.py
+++ mtpy/gis/raster_tools.py
@@ -109,3 +109,44 @@ def array2raster(
 
     # be sure to flush the data
     outband.FlushCache()
+
+
+from pathlib import Path
+import numpy as np
+from mtpy.modeling import StructuredGrid3D
+import rasterio
+from rasterio.transform import Affine
+
+fn = Path(
+    r"c:\Users\jpeacock\OneDrive - DOI\Geothermal\BuffaloValley\modem_inv\inv_01\bv_z03_t02_c03_NLCG_147.rho"
+)
+
+s = StructuredGrid3D()
+s.from_modem(fn)
+
+s.center_point.latitude = 40.275185
+s.center_point.longitude = -117.400796
+s.center_point.utm_crs = 32611
+pad = 7
+lower_left = s.get_lower_left_corner(pad, pad)
+
+transform = Affine.translation(
+    lower_left.east,
+    lower_left.north,
+) * Affine.scale(s.cell_size_east, s.cell_size_north)
+
+with rasterio.open(
+    fn.parent.joinpath("rasterio_test.tif"),
+    "w",
+    driver="GTiff",
+    height=s.res_model.shape[0] - (pad * 2),
+    width=s.res_model.shape[1] - (pad * 2),
+    count=1,
+    dtype=s.res_model.dtype,
+    crs=s.center_point.utm_crs,
+    transform=transform,
+) as dataset:
+    dataset.write(
+        np.log10(s.res_model[pad:-pad, pad:-pad, 30]),
+        1,
+    )
