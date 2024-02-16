f3abb043fa6cac8ea9a03d7a74f64a77fe15196c
JP <peacock.jared@gmail.com>
2024-02-16 11:02:24 -0800

    Update mt_data.py

--- mtpy/core/mt_data.py
+++ mtpy/core/mt_data.py
@@ -227,7 +227,7 @@ class MTData(OrderedDict, MTStations):
         """
         At this point not implemented, mainly here for inheritance of MTStations
         """
-        self.logger.waring("mt_list cannot be set.")
+        self.logger.warning("mt_list cannot be set.")
         pass
 
     @property
@@ -532,7 +532,9 @@ class MTData(OrderedDict, MTStations):
                 )
 
             else:
-                mt_data.add_station(new_mt_obj, compute_relative_location=False)
+                mt_data.add_station(
+                    new_mt_obj, compute_relative_location=False
+                )
 
         if not inplace:
             return mt_data
@@ -556,7 +558,9 @@ class MTData(OrderedDict, MTStations):
             if not inplace:
                 rot_mt_obj = mt_obj.copy()
                 rot_mt_obj.rotation_angle = rotation_angle
-                mt_data.add_station(rot_mt_obj, compute_relative_location=False)
+                mt_data.add_station(
+                    rot_mt_obj, compute_relative_location=False
+                )
             else:
                 mt_obj.rotation_angle = rotation_angle
 
@@ -654,8 +658,12 @@ class MTData(OrderedDict, MTStations):
             self.t_model_error.floor = t_floor
 
         for mt_obj in self.values():
-            mt_obj.compute_model_z_errors(**self.z_model_error.error_parameters)
-            mt_obj.compute_model_t_errors(**self.t_model_error.error_parameters)
+            mt_obj.compute_model_z_errors(
+                **self.z_model_error.error_parameters
+            )
+            mt_obj.compute_model_t_errors(
+                **self.t_model_error.error_parameters
+            )
 
     def get_nearby_stations(self, station_key, radius, radius_units="m"):
         """
