45feb7cf63467fc4f2629a234f8d8fbe616016d7
JP <peacock.jared@gmail.com>
2024-03-01 11:28:48 -0800

    update mt_data

--- mtpy/core/mt_data.py
+++ mtpy/core/mt_data.py
@@ -546,7 +546,9 @@ class MTData(OrderedDict, MTStations):
                 )
 
             else:
-                mt_data.add_station(new_mt_obj, compute_relative_location=False)
+                mt_data.add_station(
+                    new_mt_obj, compute_relative_location=False
+                )
 
         if not inplace:
             return mt_data
@@ -570,7 +572,9 @@ class MTData(OrderedDict, MTStations):
             if not inplace:
                 rot_mt_obj = mt_obj.copy()
                 rot_mt_obj.rotation_angle = rotation_angle
-                mt_data.add_station(rot_mt_obj, compute_relative_location=False)
+                mt_data.add_station(
+                    rot_mt_obj, compute_relative_location=False
+                )
             else:
                 mt_obj.rotation_angle = rotation_angle
 
@@ -668,8 +672,12 @@ class MTData(OrderedDict, MTStations):
             self.t_model_error.floor = t_floor
 
         for mt_obj in self.values():
-            mt_obj.compute_model_z_errors(**self.z_model_error.error_parameters)
-            mt_obj.compute_model_t_errors(**self.t_model_error.error_parameters)
+            mt_obj.compute_model_z_errors(
+                **self.z_model_error.error_parameters
+            )
+            mt_obj.compute_model_t_errors(
+                **self.t_model_error.error_parameters
+            )
 
     def get_nearby_stations(self, station_key, radius, radius_units="m"):
         """
@@ -901,7 +909,7 @@ class MTData(OrderedDict, MTStations):
 
         self.from_modem(data_filename, file_type=file_type, **kwargs)
 
-    def from_modem(self, data_filename, file_type="data", **kwargs):
+    def from_modem(self, data_filename, survey="data", **kwargs):
         """
         read in a modem data file
 
@@ -915,10 +923,9 @@ class MTData(OrderedDict, MTStations):
         """
         modem_data = Data(**kwargs)
         mdf = modem_data.read_data_file(data_filename)
-        if file_type in ["data"]:
-            mdf.dataframe["survey"] = "data"
-        elif file_type in ["response", "model"]:
-            mdf.dataframe["survey"] = "model"
+        # set survey name to something useful
+        mdf.dataframe["survey"] = survey
+
         self.from_dataframe(mdf.dataframe)
         self.z_model_error = ModelErrors(
             mode="impedance", **modem_data.z_model_error.error_parameters
