0da8a15a5c721933a966af738b8222c459046efd
JP <peacock.jared@gmail.com>
2024-08-20 15:18:42 -0700

    updating station locations

--- mtpy/modeling/simpeg/data.py
+++ mtpy/modeling/simpeg/data.py
@@ -8,6 +8,22 @@ Created on Fri Aug  9 12:11:57 2024
 # =============================================================================
 # Imports
 # =============================================================================
+import numpy as np
+
+from SimPEG.electromagnetics import natural_source as nsem
+from SimPEG.electromagnetics.static import utils as sutils
+from SimPEG import (
+    maps,
+    utils,
+    optimization,
+    objective_function,
+    inversion,
+    inverse_problem,
+    directives,
+    data_misfit,
+    regularization,
+    data,
+)
 
 
 # =============================================================================
@@ -19,15 +35,16 @@ class Simpeg2DData:
 
         # nez+ as keys then enz- as values
         self.component_map = {
-            "xx": "yy",
-            "xy": "yx",
-            "yx": "xy",
-            "yy": "xx",
-            "zx": "zy",
-            "zy": "zx",
+            "te": {"simpeg": "yx", "z+": "xy"},
+            "tm": {"simpeg": "xy", "z+": "yx"},
         }
 
-    def get_station_locations(self, utm=True):
+        self.include_elevation = True
+        self.invert_te = True
+        self.invert_tm = True
+
+    @property
+    def station_locations(self):
         """
         get station locations in utm geographic coordinates if True, otherwise
         will be in model coordinates.
@@ -38,16 +55,146 @@ class Simpeg2DData:
         :rtype: TYPE
 
         """
-        station_df = self.dataframe.groupby("station")
-        if utm:
-            east = station_df.east
-            north = station_df.north
-        else:
-            east = self.dataframe.model_east
-            north = self.dataframe.model_north
+        station_df = self.dataframe.groupby("station").nth(0)
 
-    def from_dataframe():
+        offsets = station_df.profile_offset
+        if np.all(offsets == 0):
+            raise ValueError("Need to calculate profile_offset.")
+
+        if self.include_elevation:
+            return np.c_[station_df.profile_offset, station_df.elevation]
+        else:
+            return np.c_[
+                station_df.profile_offset, np.zeros(station_df.shape[0])
+            ]
+
+    @property
+    def frequencies(self):
         """
+        frequencies from the data frame
+
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        return 1.0 / self.dataframe.period.unique()
+
+    def _get_mode_sources(self, simpeg_mode):
+        """
+        get mode  objects
+
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+        rx_locs = self.station_locations.copy()
+        rx_list = [
+            nsem.receivers.PointNaturalSource(
+                rx_locs,
+                orientation=simpeg_mode,
+                component="apparent_resistivity",
+            ),
+            nsem.receivers.PointNaturalSource(
+                rx_locs, orientation=simpeg_mode, component="phase"
+            ),
+        ]
+
+        src_list = [
+            nsem.sources.Planewave(rx_list, frequency=f)
+            for f in self.frequencies
+        ]
+        return nsem.Survey(src_list)
+
+    @property
+    def survey_te(self):
+        """
+        survey for TE mode (simpeg = "yx")
+
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        return self._get_mode_sources(self.component_map["te"]["simpeg"])
+
+    @property
+    def survey_tm(self):
+        """
+        survey for TM mode (simpeg = "xy")
+
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        return self._get_mode_sources(self.component_map["te"]["simpeg"])
+
+    def _get_data_observations(self, mode):
+        """
+        get data
+        :param mode: [ 'te' | 'tm' ]
+        :type simpeg_mode: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        mode = self.component_map[mode]["z+"]
+
+        return np.hstack(
+            (self.dataframe[f"res_{mode}"], self.dataframe[f"phase_{mode}"])
+        )
+
+    @property
+    def te_observations(self):
+        """
+        TE observations
+        """
+        return self._get_data_observations("te")
+
+    @property
+    def tm_observations(self):
+        """
+        TM observations
+        """
+        return self._get_data_observations("tm")
+
+    def _get_data_errors(self, mode):
+        """
+        get data
+        :param mode: [ 'te' | 'tm' ]
+        :type simpeg_mode: TYPE
+        :return: DESCRIPTION
+        :rtype: TYPE
+
+        """
+
+        mode = self.component_map[mode]["z+"]
+
+        return np.hstack(
+            (
+                self.dataframe[f"res_{mode}_model_error"],
+                self.dataframe[f"phase_{mode}_model_error"],
+            )
+        )
+
+    @property
+    def te_data_errors(self):
+        return self._get_data_errors("te")
+
+    @property
+    def tm_data_errors(self):
+        return self._get_data_errors("tm")
+
+    def _get_data(self):
+        """
+        We are assuming that the dataframe provide has a data point for each
+        period.  This can be achieved by using
+
+         >>> MTData.interpolate(new_period, bounds_error=False)
+
+        This will place Nans where there are no data and can be handeled later.
+
 
         :return: DESCRIPTION
         :rtype: TYPE
--- tests/modeling/simpeg/test_simpeg_2d.py
+++ tests/modeling/simpeg/test_simpeg_2d.py
@@ -24,15 +24,82 @@ class TestSimpeg2DData(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.md = MTData()
-        self.md.add_station(PROFILE_LIST)
+        self.md.add_station(
+            [fn for fn in PROFILE_LIST if fn.name.startswith("16")]
+        )
+        self.md.utm_epsg = 4462
 
-        self.mt_df = self.md.to_dataframe()
+        self.profile = self.md.get_profile(
+            149.15, -22.3257, 149.20, -22.3257, 1000
+        )
+
+        self.mt_df = self.profile.to_dataframe()
 
     def setUp(self):
         self.simpeg_data = Simpeg2DData(self.mt_df)
 
-    def test_get_locations(self):
-        self.mt_df
+    def test_get_locations_fail(self):
+        df = self.md.to_dataframe()
+        df.profile_offset = 0
+        s = Simpeg2DData(df)
+        self.assertRaises(ValueError, getattr, s, "station_locations")
+
+    def test_station_locations(self):
+        with self.subTest("shape"):
+            self.assertEqual((6, 2), self.simpeg_data.station_locations.shape)
+
+        with self.subTest("offset"):
+            self.assertTrue(
+                np.allclose(
+                    self.simpeg_data.station_locations[:, 0],
+                    np.array(
+                        [
+                            0.0,
+                            479.36423899,
+                            1032.47570849,
+                            1526.02107079,
+                            2005.38361755,
+                            2501.76393224,
+                        ]
+                    ),
+                )
+            )
+        with self.subTest("elevation"):
+            self.assertTrue(
+                np.allclose(
+                    self.simpeg_data.station_locations[:, 1],
+                    np.array([210.0, 213.0, 212.0, 219.0, 214.0, 220.0]),
+                )
+            )
+
+    def test_station_locations_no_elevation(self):
+        self.simpeg_data.include_elevation = False
+        with self.subTest("shape"):
+            self.assertEqual((6, 2), self.simpeg_data.station_locations.shape)
+
+        with self.subTest("offset"):
+            self.assertTrue(
+                np.allclose(
+                    self.simpeg_data.station_locations[:, 0],
+                    np.array(
+                        [
+                            0.0,
+                            479.36423899,
+                            1032.47570849,
+                            1526.02107079,
+                            2005.38361755,
+                            2501.76393224,
+                        ]
+                    ),
+                )
+            )
+        with self.subTest("elevation"):
+            self.assertTrue(
+                np.allclose(
+                    self.simpeg_data.station_locations[:, 1],
+                    np.zeros((6)),
+                )
+            )
 
 
 # =============================================================================
