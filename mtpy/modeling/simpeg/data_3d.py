de58e550f03de874d195f391870730281777638b
JP <peacock.jared@gmail.com>
2024-08-22 12:21:28 -0700

    adding data_3d

---
+++ mtpy/modeling/simpeg/data_3d.py
@@ -0,0 +1,63 @@
+# =============================================================================
+# Imports
+# =============================================================================
+import warnings
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
+from simpeg.electromagnetics import natural_source as nsem
+from simpeg import (
+    maps,
+    optimization,
+    inversion,
+    inverse_problem,
+    directives,
+    data_misfit,
+    regularization,
+)
+from pymatsolver import Pardiso
+
+# from dask.distributed import Client, LocalCluster
+from mtpy.modeling.simpeg.data_2d import Simpeg2DData
+from mtpy.modeling.simpeg.make_2d_mesh import QuadTreeMesh
+
+warnings.filterwarnings("ignore")
+
+
+# =============================================================================
+
+class Simpeg3DData:
+    """ """
+
+    def __init__(self, dataframe, **kwargs):
+        self.dataframe = dataframe
+
+        # nez+ as keys then enz- as values
+        self.component_map = {
+            "xx": {"simpeg": "yy", "z+": "xx"},
+            "xy": {"simpeg": "yx", "z+": "xy"},
+            "yx": {"simpeg": "xy", "z+": "yx"},
+            "yy": {"simpeg": "xx", "z+": "yy"},
+            "zx": {"simpeg": "zy", "z+": "zx"},
+            "zy": {"simpeg": "zx", "z+": "zy"},
+        }
+
+        self.include_elevation = False
+        self.topography = None # should be a geotiff or asc file
+        self.geographic_coordinates = True
+
+    @property
+    def station_locations(self):
+        """
+        return just station locations in appropriate coordinates, default is
+        geographic.
+        """
+        if self.geographic_coordinates:
+            station_df = self.dataframe.groupby("station").nth(0)
+
+            if self.include_elevation:
+                return np.c_[station_df.east, station_df.north, station_df.elevation]
+            else:
+                return np.c_[station_df.east, station_df.north, np.zeros(station_df.elevation.size)]
