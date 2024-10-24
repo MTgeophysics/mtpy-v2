643ceacc6864cfa7ba0e9a4e4c3d9b3ff293413c
JP <peacock.jared@gmail.com>
2024-10-23 20:22:01 -0700

    Bump version: 2.0.11 → 2.0.12

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/MTgeophysics/mtpy-v2",
-    version="2.0.11",
+    version="2.0.12",
     zip_safe=False,
     package_data={"": []},
 )
